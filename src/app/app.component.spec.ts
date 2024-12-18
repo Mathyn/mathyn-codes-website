3cf1c90678a5134ffb1aa1237fde679d645618fe
Mathyn <git@mathyn.com>
2024-12-18 16:56:55 +0100

    Added basic linter
    Also added single quotes for strings as a rule

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
         expect(app).toBeTruthy();
     });
 
-    it(`should have the 'mathyn-codes-website' title`, () => {
+    it('should have the \'mathyn-codes-website\' title', () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.componentInstance;
         expect(app.title).toEqual('mathyn-codes-website');
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
     styleUrl: './app.component.scss'
 })
 export class AppComponent {
-    title = 'mathyn-codes-website';
+    title = 'mathyn-codes-website'
 }
